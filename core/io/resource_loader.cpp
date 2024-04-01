4a7dadfcf49f2346d9526e4f22c02a83714641a5
ajreckof <tbonhoure@ymail.com>
2024-04-01 09:34:13 +0200

    Alternative fix ResourceLoader.load cache with relative paths.

--- core/io/resource_loader.cpp
+++ core/io/resource_loader.cpp
@@ -398,13 +398,15 @@ void ResourceLoader::_thread_load_function(void *p_userdata) {
 }
 
 static String _validate_local_path(const String &p_path) {
-	ResourceUID::ID uid = ResourceUID::get_singleton()->text_to_id(p_path);
+	String path = p_path;
+	if (p_path.is_relative_path()) {
+		path = "res://" + p_path;
+	}
+	ResourceUID::ID uid = ResourceUID::get_singleton()->text_to_id(path);
 	if (uid != ResourceUID::INVALID_ID) {
 		return ResourceUID::get_singleton()->get_id_path(uid);
-	} else if (p_path.is_relative_path()) {
-		return "res://" + p_path;
 	} else {
-		return ProjectSettings::get_singleton()->localize_path(p_path);
+		return ProjectSettings::get_singleton()->localize_path(path);
 	}
 }
 
